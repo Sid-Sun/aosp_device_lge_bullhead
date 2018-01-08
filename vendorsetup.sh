de5c0302f2580ad59532f737567830251975f211
Sid-Sun <sidharth.soni525@gmail.com>
2018-01-08 20:33:34 +0530

    Cosmic-ify
    Signed-off-by: Sid-Sun <sidharth.soni525@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_bullhead-userdebug
-add_lunch_combo aosp_bullhead_svelte-userdebug
+add_lunch_combo cos_bullhead-userdebug
+#add_lunch_combo aosp_bullhead_svelte-userdebug
